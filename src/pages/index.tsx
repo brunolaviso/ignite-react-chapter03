5b321831e672e10843fe000b022e9f0143385fea
Bruno Laviso <bruno.laviso@hotmail.com>
2021-03-26 13:46:31 -0300

    feat: add position on button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
 
 import styles from './home.module.scss'
 
@@ -14,8 +15,9 @@ export default function Home() {
           <h1>News about the <span>React</span> world.</h1>
           <p>
             get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for $9.90 month</span>.
           </p>
+          <SubscribeButton />
         </section>
         <img src="./images/avatar.svg" alt="Girl coding"/>
       </main>
